4222fabc53abf6a31962363062ff0afc0e1b4776
ksm0207 <ksm03071@naver.com>
2021-01-13 06:09:25 +0900

    Python 알고리즘 문제 풀이 완료
    - 좌표정렬

---
+++ Part #5/example06.py
@@ -0,0 +1,26 @@
+# 문제
+# 2차원 평면 위의 점 N개가 주어진다. 좌표를 y좌표가 증가하는 순으로, y좌표가 같으면 x좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다.
+# 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
+
+# 출력
+# 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+
+
+num = int(input())
+array = []
+
+for i in range(num):
+    x, y = list(map(int, input().split()))
+    array.append((x, y))
+
+array.sort(key=lambda y: (y[1], y[0]))
+print("lambda = ", array)
+
+for j in array:
+
+    print("R = ", j[0], j[1])
+
+# https://dailyheumsi.tistory.com/67
