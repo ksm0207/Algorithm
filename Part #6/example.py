b0b35d01ac3c735b642b5738987656c83826401b
ksm0207 <ksm03071@naver.com>
2021-02-03 14:14:16 +0900

    Ready

--- Part #6/example.py
+++ Part #6/example.py
@@ -8,15 +8,16 @@
 # 주어진 수들 중 소수의 개수를 출력한다.
 
 
-n = int(input())
+num_input = int(input())
 numbers = map(int, input().split())
-sosu = 0
+result = 0
+
 for num in numbers:
-    error = 0
+    value = 0
     if num > 1:
         for i in range(2, num):  # 2부터 n-1까지
             if num % i == 0:
-                error += 1  # 2부터 n-1까지 나눈 몫이 0이면 error가 증가
-        if error == 0:
-            sosu += 1  # error가 없으면 소수.
-print(sosu)
+                value += 1  # 2부터 n-1까지 나눈 몫이 0이면 error가 증가
+        if value == 0:
+            result += 1  # error가 없으면 소수.
+print(result)
